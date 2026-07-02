4e891657ed807410cf7f37c13f709544c20309c3
Josefin Wall <josefin1010wall@gmail.com>
2026-07-02 16:37:06 +0200

    Handle errors with fetch. Style page.

--- src/main.ts
+++ src/main.ts
@@ -7,18 +7,39 @@ interface ResponseData {
 
 interface Dom {
   button: HTMLButtonElement;
+  status: HTMLParagraphElement;
   image: HTMLImageElement;
 }
 
 const app: Dom = {
   button: document.getElementById("get-dog-button") as HTMLButtonElement,
+  status: document.getElementById("status") as HTMLParagraphElement,
   image: document.getElementById("good-dog-image") as HTMLImageElement,
 };
 
+const handleError = (error: Error | unknown) => {
+  if (error instanceof Error) {
+    app.status.textContent = `LOST \u{1F415} ${error.message}`;
+    app.status.classList.add("error-message");
+  } else {
+    app.status.textContent = `Threw something other than an Error.`;
+  }
+};
+
 const getGoodDog = async (): Promise<void> => {
-  const response: Response = await fetch(API_URL);
-  const goodDog: ResponseData = (await response.json()) as ResponseData;
-  app.image.src = goodDog.message;
+  app.status.textContent = "Loading...";
+  app.status.classList.remove("error-message");
+  try {
+    const response: Response = await fetch(API_URL);
+    if (!response.ok) {
+      throw new Error(`${response.status}`);
+    }
+    const goodDog: ResponseData = (await response.json()) as ResponseData;
+    app.image.src = goodDog.message;
+    app.status.textContent = "";
+  } catch (error) {
+    handleError(error);
+  }
 };
 
 app.button.addEventListener("click", () => {
